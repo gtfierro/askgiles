1f693e6dd3443091c93a778ab8feb350f9413fac
Gabe Fierro <gtfierro225@gmail.com>
2015-11-22 12:05:02 -0800

    Initial commit with basic query functionality
    This doesn't have nearly any of the features I want to add

---
+++ README.md
@@ -0,0 +1,9 @@
+# AskGiles
+
+Drop-down binary tool for querying and performing common operations on a Giles server.
+
+
+TODO:
+- command line visualizations! histograms, sparklines, heatmaps, etc
+- Make this integratable with other command line tools
+- finish the time formatting
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+    "net/http"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "os"
+    "bytes"
+)
+
+
+var host string
+var formatTime  bool
+var timeFormat  string
+
+var (
+    MIME_TEXT   =   "text/plain"
+    MIME_JSON   =   "application/json"
+)
+
+func init() {
+    flag.StringVar(&host, "h", "http://localhost", "Host to direct queries to")
+    flag.BoolVar(&formatTime, "t", true, "If true, formats time according to RFC3339. Can specify ANSIC, RFC822, RFC1123, RFC3339 using the -f flag. If false, displays the unix-style timestamp.")
+    flag.StringVar(&timeFormat, "f", "RFC3339", "Time format. Can specify ANSIC, RFC822, RFC1123, RFC3339.")
+}
+
+func prettyPrintJSON(v interface{}) {
+	if b, err := json.MarshalIndent(v, "", "  "); err != nil {
+		fmt.Println("ERROR FORMATTING (%v) %v", err, v)
+	} else {
+		fmt.Println(string(b))
+	}
+}
+
+func handleQuery(args []string) {
+    var (
+        query   string
+        buf     *bytes.Buffer
+    )
+
+    if len(args) < 1 {
+        fmt.Println("No query specified")
+        os.Exit(1)
+    }
+
+    query = args[0]
+    buf = bytes.NewBufferString(query)
+
+    resp, err := http.Post(host, MIME_TEXT, buf)
+    if err != nil {
+        fmt.Printf("Error running query %v: %v\n", query, err)
+        os.Exit(1)
+    }
+
+    // expecting json
+    var data []interface{}
+    var decoder = json.NewDecoder(resp.Body)
+    decoder.Decode(&data)
+    resp.Body.Close()
+
+    prettyPrintJSON(data)
+}
+
+func main() {
+    flag.Parse()
+
+    args := flag.Args()
+    if len(args) == 0 {
+        fmt.Println(`
+Need to specify a command! One of:
+    query   -   Send a query to a Giles instance
+`)
+        os.Exit(1)
+    }
+    subcommand := args[0]
+    switch subcommand {
+    case "query":
+        handleQuery(args[1:])
+    default:
+        fmt.Printf("Subcommand %v not found\n", subcommand[0])
+        os.Exit(1)
+    }
+}
