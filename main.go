2e838c6114b3983ce46343effe523beb88e02721
Gabe Fierro <gtfierro225@gmail.com>
2015-11-22 16:49:45 -0800

    Add some basic dataviz
    Using sparklines for basic data visualization. Probably going to do
    histograms, regular JSOn output, etc

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	giles "github.com/gtfierro/giles/archiver"
-	"net/http"
 	"os"
 )
 
@@ -25,41 +21,32 @@ func init() {
 	flag.StringVar(&timeFormat, "f", "RFC3339", "Time format. Can specify ANSIC, RFC822, RFC1123, RFC3339.")
 }
 
-func prettyPrintJSON(v interface{}) {
-	if b, err := json.MarshalIndent(v, "", "  "); err != nil {
-		fmt.Println("ERROR FORMATTING (%v) %v", err, v)
-	} else {
-		fmt.Println(string(b))
-	}
-}
-
 func handleQuery(args []string) {
-	var (
-		query string
-		buf   *bytes.Buffer
-	)
-
 	if len(args) < 1 {
 		fmt.Println("No query specified")
 		os.Exit(1)
 	}
 
-	query = args[0]
-	buf = bytes.NewBufferString(query)
+	data := doQuery(args[0])
 
-	resp, err := http.Post(host, MIME_TEXT, buf)
-	if err != nil {
-		fmt.Printf("Error running query %v: %v\n", query, err)
+	prettyPrintJSON(data)
+}
+
+func handleVis(args []string) {
+	if len(args) < 1 {
+		fmt.Println("No query specified")
 		os.Exit(1)
 	}
 
-	// expecting json
-	var data []giles.SmapMessage
-	var decoder = json.NewDecoder(resp.Body)
-	decoder.Decode(&data)
-	resp.Body.Close()
+	data := doQuery(args[0])
 
-	prettyPrintJSON(data)
+	visType := args[1]
+	switch visType {
+	case "spark":
+		visSpark(data)
+	default:
+		visSpark(data)
+	}
 }
 
 func main() {
@@ -77,6 +64,8 @@ Need to specify a command! One of:
 	switch subcommand {
 	case "query":
 		handleQuery(args[1:])
+	case "vis":
+		handleVis(args[1:])
 	default:
 		fmt.Printf("Subcommand %v not found\n", subcommand[0])
 		os.Exit(1)
